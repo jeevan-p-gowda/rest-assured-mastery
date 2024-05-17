2146fe4b1885061314ab4757f216cc9f82c0fc21
jeevan-p-gowda <jeevanputtaswamy@gmail.com>
2024-05-18 00:26:45 +0530

    no auth test

--- src/test/java/GetTest.java
+++ src/test/java/GetTest.java
@@ -1,6 +1,9 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GetTest {
     @Test
@@ -16,5 +19,18 @@ public class GetTest {
 
         // Log the status code
         System.out.println("Status Code: " + statusCode);
+    }
+
+    @Test
+    public void productWithoutAuthHeader() {
+        // Specify the URL for the API call
+        RestAssured.baseURI = "https://www.apicademy.dev/";
+
+        // Send a GET request and get the response
+        Response response = RestAssured.given().get("/api/products/");
+
+        // Non-BDD Assertions using Hamcrest
+        assertThat(response.getStatusCode(), Matchers.is(400));
+        assertThat(response.jsonPath().getString("message"), Matchers.equalTo("Authorization header is missing."));
     }
 }
